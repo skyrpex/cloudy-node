997d3b2c605274b2c2e82b6b8e77f4cfadc65569
Cristian Pallarés <cristian.pallares@virtualtools.com.au>
2022-05-09 17:57:23 +0200

    chore: approve upgrades automatically (#15)

--- .projenrc.js
+++ .projenrc.js
@@ -21,6 +21,8 @@ const project = new typescript.TypeScriptProject({
   minNodeVersion: "14.18.0",
   prettier: true,
   releaseToNpm: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {},
 });
 
 // Use ESM. See https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c#how-can-i-move-my-commonjs-project-to-esm.
