7d930768dae3d5c56505523c5adbf43491505121
Cristian Pallarés <skyrpex@gmail.com>
2022-05-11 17:34:39 +0200

    feat: add compatibility with yarn pnp esm (#20)
    In order for `cloudy-node` to work with Yarn PNP + ESM, we have to provide a CommonJS module. When consuming `cloudy-node` using that environment, we also need to use `yarn unplug cloudy-node`.

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-import { typescript, github } from "projen";
+const { typescript, github } = require("projen");
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: "main",
@@ -35,12 +35,11 @@ const project = new typescript.TypeScriptProject({
 for (const tsconfig of [project.tsconfig, project.tsconfigDev]) {
   tsconfig.file.addOverride("compilerOptions.lib", ["ES2020"]);
   tsconfig.file.addOverride("compilerOptions.target", "ES2020");
-  tsconfig.file.addOverride("compilerOptions.module", "ES2020");
+  tsconfig.file.addOverride("compilerOptions.module", "CommonJS");
   tsconfig.file.addOverride("compilerOptions.moduleResolution", "node");
 }
 project.addDevDeps("@types/node@^14.0.0");
 project.addFields({
-  type: "module",
   exports: {
     ".": {
       import: `./lib/index.js`,
@@ -49,7 +48,7 @@ project.addFields({
   },
 });
 
-project.compileTask.prependExec(
+project.compileTask.exec(
   `yarn link && cd ./test/test-app && yarn link ${project.package.packageName}`
 );
 project.testTask.prependExec("cd ./test/test-app && yarn");
